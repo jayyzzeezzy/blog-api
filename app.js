94658f2feeea9f06781bf2af4f9497b726cb8041
James Zhou <jam9es@gmail.com>
2025-08-08 12:40:28 -0700

    Set up the backend by installing the necessary dependencies

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require('cors');
+
+const app = express();
+// cors
+app.use(cors());
+// Express body-parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
